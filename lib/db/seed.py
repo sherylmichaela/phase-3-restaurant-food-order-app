75b7a62223e2502d1604c04763748078c9ea6406
Sheryl Chee <sherylmichaela@gmail.com>
2024-05-24 00:37:34 +1000

    Restructure code in seed.py file

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -2,40 +2,45 @@ from faker import Faker
 from faker.providers import BaseProvider
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Customer, MenuItem, Order
-
-engine = create_engine("sqlite:///data.db")
-Session = sessionmaker(bind=engine)
-session = Session()
-
-fake = Faker()
-
-# Registering AustralianMobileProvider
-class AustralianMobileProvider(BaseProvider):
-        def mobile_number(self):
-              return "04" + str(self.random_number(digits=8)).zfill(8)
-        
-fake.add_provider(AustralianMobileProvider)
-
-
-def delete_records():
-      session.query(Customer).delete()
-      session.query(MenuItem).delete()
-      session.query(Order).delete()
+from models import Base, Customer, MenuItem, Order
 
 if __name__ == "__main__":
-        delete_records()
+    engine = create_engine("sqlite:///data.db")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# Customers
-for i in range (100):
-    customer = Customer(
-        first_name=fake.first_name(), 
-        last_name=fake.last_name(),
-        mobile=fake.mobile_number()
-        )
-    session.add(customer)
-    session.commit()
+    fake = Faker()
 
-print("Seeding successful for customers data")
+    # Delete records from Customer, MenuItem and Order tables
+    def delete_records():
+        session.query(Customer).delete()
+        session.query(MenuItem).delete()
+        session.query(Order).delete()
 
-# Menu Items
+    delete_records()
+
+    # Create and register custom AustralianMobileProvider
+    class AustralianMobileProvider(BaseProvider):
+            def mobile_number(self):
+                return "04" + str(self.random_number(digits=8)).zfill(8)
+            
+    fake.add_provider(AustralianMobileProvider)
+
+    # Customers
+    for i in range (100):
+        customer = Customer(
+            first_name=fake.first_name(), 
+            last_name=fake.last_name(),
+            mobile=fake.mobile_number()
+            )
+        session.add(customer)
+        session.commit()
+
+    print("Seeding successful for customers data")
+
+    # Menu Items
+
+
+
+    # Orders
